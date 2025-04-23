dd51a56eea0bfddf9d81b507bc168903678cf3e2
Jaewon Oh <107977555+Ohjason-1@users.noreply.github.com>
2025-04-23 16:39:37 -0700

    done

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
     setLoading(true);
     try {
       // This URL will need to be updated to your deployed backend URL
-      const response = await fetch('http://localhost:5000/api/quote');
+      const response = await fetch('http://127.0.0.1:5000/api/quote');
       const data = await response.json();
       setQuote(data.quote);
     } catch (error) {
